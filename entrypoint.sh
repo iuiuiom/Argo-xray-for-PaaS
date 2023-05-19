628bf4f0b1d484c4bf6920792c251ff45bc10ba3
Corrine <129360343+iuiuiom@users.noreply.github.com>
2023-05-19 00:13:05 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
-UUID=${UUID:-'7dff8d05-62d6-4fe3-b728-599ddd03dbee8'}
+UUID=${UUID:-'7dff8d05-62d6-4fe3-b728-599ddd03dbee'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/argo-vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/argo-vless'}
 TROJAN_WSPATH=${TROJAN_WSPATH:-'/argo-trojan'}
